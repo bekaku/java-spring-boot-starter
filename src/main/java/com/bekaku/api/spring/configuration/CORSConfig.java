bd5e78dc5f1bdebec308db182d41b631dea9651a
bekaku <33171470+bekaku@users.noreply.github.com>
2025-06-03 14:02:51 +0700

    updated CORS config

--- src/main/java/com/bekaku/api/spring/configuration/CORSConfig.java
+++ src/main/java/com/bekaku/api/spring/configuration/CORSConfig.java
@@ -1,12 +1,21 @@
 package com.bekaku.api.spring.configuration;
 
+import com.bekaku.api.spring.properties.AppProperties;
+import com.bekaku.api.spring.properties.CorsProperties;
 import com.bekaku.api.spring.util.ConstantData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class CORSConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private CorsProperties corsProperties;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -22,7 +31,22 @@ public class CORSConfig implements WebMvcConfigurer {
                 )
                 .exposedHeaders(ConstantData.CONTENT_DISPOSITION)
                 .allowCredentials(false)
-                .allowedOriginPatterns("*");
-//                .allowedOrigins("http://localhost:9100");
+                .allowedOrigins(corsProperties.getAllowedOrigins().toArray(new String[0]))
+                .maxAge(3600);
+//                .allowedOriginPatterns("*");
+//                .allowedOrigins(
+//                        "http://localhost:8080",    // Dev
+//                        "capacitor://localhost",    // Capacitor IOS
+//                        "http://localhost", // Capacitor Android
+//                        "https://192.168.1.100"      // local IP บน LAN
+//                );
+
+
+        // onother mapping for /public/** have diference allowedOrigins
+        /*
+        registry.addMapping("/public/**")
+                .allowedOrigins("https://another-public-domain.com", "http://another-dev-domain.com")
+                .maxAge(3600);
+         */
     }
 }
