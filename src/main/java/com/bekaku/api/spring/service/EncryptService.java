eeb8f07c6f420309a43c4d7b209992f4ab3aafa1
bekaku <33171470+bekaku@users.noreply.github.com>
2025-06-11 14:00:43 +0700

    .

--- src/main/java/com/bekaku/api/spring/service/EncryptService.java
+++ src/main/java/com/bekaku/api/spring/service/EncryptService.java
@@ -1,8 +1,16 @@
 package com.bekaku.api.spring.service;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
 public interface EncryptService {
 
     String encrypt(String password, String salt);
     boolean check(String checkPassword, String realPassword);
+    String encrypt(String data) throws IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException;
+    String decrypt(String encryptedData) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException;
 
 }
--- src/main/java/com/bekaku/api/spring/serviceImpl/EncryptServiceImpl.java
+++ src/main/java/com/bekaku/api/spring/serviceImpl/EncryptServiceImpl.java
@@ -2,11 +2,26 @@ package com.bekaku.api.spring.serviceImpl;
 
 import com.bekaku.api.spring.service.EncryptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
 @Service("encryptService")
 public class EncryptServiceImpl implements EncryptService {
+
+    private static final String ALGORITHM = "AES";
+    //    private static final byte[] key = "jOwttyTbN/16DI5iIT0FMg==".getBytes(); // Use AES-128 key a 128-bit key
+    @Value("${app.encrypt-key}")
+    String encryptKey;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -22,4 +37,28 @@ public class EncryptServiceImpl implements EncryptService {
 //        return checkPassword.equals(realPassword);
         return passwordEncoder.matches(checkPassword, realPassword);
     }
+
+    public byte[] getEncryptKey() {
+        return encryptKey.getBytes();
+    }
+
+    @Override
+    public String encrypt(String data) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        SecretKeySpec secretKey = new SecretKeySpec(getEncryptKey(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedData = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedData);
+    }
+
+    @Override
+    public String decrypt(String encryptedData) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        SecretKeySpec secretKey = new SecretKeySpec(getEncryptKey(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decodedData = Base64.getDecoder().decode(encryptedData);
+        byte[] decryptedData = cipher.doFinal(decodedData);
+        return new String(decryptedData);
+    }
+
 }
