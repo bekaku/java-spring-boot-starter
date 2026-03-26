cd886d64db577fcff8262ffd7bbd6f633f65393f
bekaku <33171470+bekaku@users.noreply.github.com>
2026-03-26 16:41:11 +0700

    Update AppHints.java

--- src/main/java/com/bekaku/api/spring/configuration/AppHints.java
+++ src/main/java/com/bekaku/api/spring/configuration/AppHints.java
@@ -6,12 +6,19 @@ import com.bekaku.api.spring.mybatis.AppUserMybatis;
 import com.bekaku.api.spring.mybatis.FileManagerMybatis;
 import com.bekaku.api.spring.mybatis.FilesDirectoryMybatis;
 import com.bekaku.api.spring.mybatis.PermissionMybatis;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.aot.hint.TypeReference;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
 
 public class AppHints implements RuntimeHintsRegistrar {
+
+    private static final String MAPPER_PACKAGE = "com.bekaku.api.spring.mybatis";
+    private static final String DTO_PACKAGE = "com.bekaku.api.spring.dto";
     @Override
     public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
         // บอกให้ GraalVM ก๊อปปี้ทุกไฟล์ในโฟลเดอร์ i18n เข้าไปใน Native Image ด้วย
@@ -36,28 +43,29 @@ public class AppHints implements RuntimeHintsRegistrar {
         // hints.resources().registerPattern("static/img/**/*.png");
         // hints.resources().registerPattern("static/img/**/*.jpg");
         // hints.resources().registerPattern("static/img/**/*.svg");
+
         hints.reflection().registerType(
                 org.apache.ibatis.logging.slf4j.Slf4jImpl.class,
                 MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS
         );
-        hints.proxies().registerJdkProxy(
-                AccessTokenMybatis.class
-        );
-        hints.proxies().registerJdkProxy(
-                AppRoleMybatis.class
-        );
-        hints.proxies().registerJdkProxy(
-                AppUserMybatis.class
-        );
-        hints.proxies().registerJdkProxy(
-                FileManagerMybatis.class
-        );
-        hints.proxies().registerJdkProxy(
-                FilesDirectoryMybatis.class
-        );
-        hints.proxies().registerJdkProxy(
-                PermissionMybatis.class
-        );
+        registerMappers(hints);
     }
+
+    private void registerMappers(RuntimeHints hints) {
+        var provider = new ClassPathScanningCandidateComponentProvider(false);
+        provider.addIncludeFilter(new AnnotationTypeFilter(Mapper.class));
+        provider.findCandidateComponents(MAPPER_PACKAGE).forEach(bd -> {
+            try {
+                Class<?> cls = Class.forName(bd.getBeanClassName());
+                hints.reflection().registerType(cls,
+                        MemberCategory.INVOKE_PUBLIC_METHODS,
+                        MemberCategory.INVOKE_DECLARED_METHODS);
+                hints.proxies().registerJdkProxy(cls);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
 }
 
